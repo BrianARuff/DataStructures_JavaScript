50f46f67f4fc84a67465c3a55ee79b675fdce0fe
BrianARuff <thelonewolf198927@gmail.com>
2018-11-26 19:22:32 -0500

    Default Graph

---
+++ Graphs/Graph.js
@@ -0,0 +1,36 @@
+class Vertex {
+    constructor(name) {
+        this.name = name
+        this.edges = [];
+    }
+    print() {
+        console.log(this);
+    }
+}
+
+class Graph {
+    constructor(name) {
+        this.name = name
+        this.vertices = [];
+    }
+    add_vertex(name) {
+        this.vertices.push(new Vertex(name));
+    }
+    add_edge(vert, edge) {
+        this.vertices.forEach(v => {
+            if(v.name === vert) {
+                return v.edges.push(edge);
+            }
+        })
+    }
+    print() {
+        console.log(this.name);
+        console.log(this.vertices);
+    }
+}
+
+g = new Graph("Graph");
+g.add_vertex("McAdenville")
+g.add_vertex("Cramerton")
+g.add_edge("McAdenville", "Cramerton");
+g.print()
